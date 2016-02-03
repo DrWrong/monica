48f86696724dd15cdd2625acb56339a40b40ddc7
drwrong <yuhangchaney@gmail.com>
2016-02-03 14:02:47 +0800

    update

---
+++ core/context.go
@@ -0,0 +1,57 @@
+package core
+
+import (
+	"monica/core/inject"
+)
+
+type Handler interface{}
+
+// the base class for the web framework
+// it provide a context for each request
+type Context struct {
+	inject.Injector
+	handlers []Handler
+	index    int
+	Req      *http.Request
+	Resp     http.ResponseWriter
+	// Data is used for a json response which
+	Data map[string]interface{}
+	// a bool value which control wheather will go on processing
+	stopProcess bool
+}
+
+// stop process
+func (c *Context) Stop() {
+	c.stopProcess = true
+}
+
+// yield another handler
+func (c *Context) Next() {
+	c.index += 1
+	c.run()
+}
+
+func (c *Context) run() {
+	for c.index < len(c.handlers) {
+		if c.stopProcess {
+			break
+		}
+
+		_, err := c.Invoke(c.handlers[c.index])
+
+		if err != nil {
+			panic(err)
+		}
+		c.index += 1
+	}
+}
+
+func NewContext(resp http.ResponseWriter, req *http.Request) *Context {
+	c := &Context{
+		Injector: inject.New(),
+		Req:      req,
+		Resp:     resp,
+	}
+	c.Map(c)
+	return c
+}
